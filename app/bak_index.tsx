57f4f261a2f4df4ab1564b65437f27d5889dfa3f
koeyismyusername <koey2182@gmail.com>
2025-04-25 10:38:42 +0900

    웹소켓 테스트용 컴포넌트 사용 제거

--- app/bak_index.tsx
+++ app/bak_index.tsx
@@ -3,7 +3,6 @@ import { Stack, Link } from 'expo-router';
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
-import NcfSubscriberTestComponent from '~/lib/websocket/NcfSubscriberTestComponent';
 
 export default function Home() {
   return (
@@ -11,7 +10,6 @@ export default function Home() {
       <Stack.Screen options={{ title: 'Home' }} />
       <Container>
         <ScreenContent path="app/index.tsx" title="Home" />
-        <NcfSubscriberTestComponent />
         <Link href={{ pathname: '/details', params: { name: 'Danny' } }} asChild>
           <Button title="Show Details" />
         </Link>
